449af046bf2efa7c1657e6787420d09995366194
eucligame <windsey@berkeley.edu>
2025-05-30 03:34:53 +0000

    Revert normal checking

--- skythought/skythought_evals/tasks/math/math_handler.py
+++ skythought/skythought_evals/tasks/math/math_handler.py
@@ -14,11 +14,10 @@ class MathTaskHandler(TaskHandler):
         return self.task_config.templating_parameters["template"].format(**problem)
 
     def check_correctness(self, problem, generation):
-        return False
-        # answer = strip_answer_string(problem[self.task_config.answer_key])
-        # pred = extract_answer(generation)
-        # pred = strip_answer_string(pred)
-        # return math_equal(pred, answer)
+        answer = strip_answer_string(problem[self.task_config.answer_key])
+        pred = extract_answer(generation)
+        pred = strip_answer_string(pred)
+        return math_equal(pred, answer)
 
     def update_results(self, problem, response):
         if not isinstance(response, str):
